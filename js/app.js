814efd475c269220c321e238b1085418b7f8dab7
reloadercfm <elcfm1@gmail.com>
2019-02-17 12:44:54 -0600

    object constructor

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
+//object literal
 const persona={
     nombre:'carlos',
     profesion:'Programador',
     edad:'50'
 }
-const persona2={
-    nombre:'fernando',
-    profesion:'tester',
-    edad:'200'
+// console.log(persona);
+
+//object constructor
+function Tarea(nombre,urgencia){
+    this.nombre=nombre;
+    this.urgencia=urgencia;
 }
-console.log(persona);
-console.log(persona2);
+//crear una nueva tarea
+//las ventajas de el object constructor es que te permite crear multiples objetos
+const tarea1= new Tarea('usar javascript', 'urgente');
+const tarea2= new Tarea('tomar cafe', 'mediano');
+const tarea3= new Tarea('pasear al perro', 'baja');
+console.log(tarea1);
+console.log(tarea1.nombre);
+
+
+console.log(tarea2);
+console.log(tarea3);
