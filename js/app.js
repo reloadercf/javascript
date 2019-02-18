0a95cfd2a5f60dd0f3a7b2ae70698269be8ae526
reloadercfm <elcfm1@gmail.com>
2019-02-18 17:43:10 -0600

    heredando clases a otra clase

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
-//escribir clases
+//herencia
 class Tarea{
     constructor(nombre,prioridad){
         this.nombre=nombre;
         this.prioridad=prioridad
     }
     mostrar(){
-        return `${this.nombre} tiene una prioridad de ${this.prioridad}`;
+       console.log(`${this.nombre} tiene una prioridad de ${this.prioridad}`);
     }
 }
 
+class ComprasPendientes extends Tarea{
+    constructor(nombre,prioridad,cantidad){
+        super(nombre,prioridad);
+        this.cantidad=cantidad
+    }
+    mostrar(){
+        super.mostrar();
+        console.log(` la cantidad de ${this.cantidad}`);
+    }
+    hola(){
+        return 'hola';
+    }
+}
+
+
 let tarea1  =   new Tarea('usar javascript','alta');
 
-console.log(tarea1.mostrar());
+// console.log(tarea1.mostrar());
+
+//Compras 
+
+let compra1 =   new ComprasPendientes('Jabon','urgente',3);
+compra1.mostrar();
+
+console.log(compra1.hola());
