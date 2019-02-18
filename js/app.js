4d09c883ca80bdb8c762ff34356f879a2f2efdef
reloadercfm <elcfm1@gmail.com>
2019-02-18 15:15:19 -0600

    ejemplo de un promise

--- js/app.js
+++ js/app.js
@@ -1,34 +1,18 @@
-//los arreglos llevan corrchetes
-const personas=[
-    {nombre:'carlos', edad:27, aprendiendo:'javascript'},
-    {nombre:'luis', edad:28, aprendiendo:'java'},
-    {nombre:'fernando', edad:30, aprendiendo:'python'},
-    {nombre:'enrrique', edad:23, aprendiendo:'angular'},
-    {nombre:'el chavo', edad:20, aprendiendo:'php'},
-]
-console.log(personas);
+//promises
 
-//mayores a 28
-const mayores=personas.filter(persona=>{
-    return persona.edad>28;
-})
-// console.log(mayores);
-
-//que aprende fernando y su edad
-
-const fernando=personas.find(persona=>{
-    return persona.nombre==='fernando';
+const aplicarDescuento = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        let descuento = false;
+        if(descuento){
+            resolve('Descuento aplicado');
+        } else{
+            reject('No se pudo aplicar el descuento')
+        }
+    }, 3000);
 });
-// console.log(`Fernando esta aprendiendo ${fernando.aprendiendo}`);
 
-//reduce nos hace la sumatoria
-let total = personas.reduce((edadTotal,persona)=>{
-    return edadTotal+persona.edad;
-},0);
-
-//suma las edades
-console.log(total);
-
-
-//cuenta los registros
-console.log(personas.length);
+aplicarDescuento.then(resultado=>{
+    console.log(resultado);
+}).catch(error=>{
+    console.log(error);
+})
